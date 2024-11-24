112ca5099e62a0715c4a589759d97a45e15b39e4
Ayaz Ahmed <ayaz.ahmed@emumba.com>
2024-11-25 01:04:39 +0500

    Added responsiveness in the main page and the other pages

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -7,64 +7,48 @@ import image3 from "../../assets/image-3.svg";
 import "./LandingPage.css";
 const LandingPage = () => {
   return (
-    <div className="landing-page-container">
-      <Box className="landing-page-left-container" sx={{ mt: 8 }}>
-        <Typography className="landing-page-heading">
-          <span className="rainbow-underline">Explore</span> The New World With
-          Tourbay
-        </Typography>
+    <>
+      <div className="outer-wrapper"></div>
+      <div className="landing-page-container">
+        <Box className="landing-page-left-container" sx={{ mt: 8 }}>
+          <Typography className="landing-page-heading">
+            <span className="rainbow-underline">Explore</span> The New World
+            With Tourbay
+          </Typography>
 
-        <Typography className="landing-page-paragraph">
-          No matter where in the world you want to go, we can help get you there
-          and make your tour a stupendous memory.
-        </Typography>
+          <Typography className="landing-page-paragraph">
+            No matter where in the world you want to go, we can help get you
+            there and make your tour a stupendous memory.
+          </Typography>
 
-        <Link to={"/explore-tour"}>
-          <Defaultbutton
-            buttonCaption="Explore Now"
-            buttonWidth="155px"
-            buttonHeight="54px"
-            route="/"
-          />
-        </Link>
-      </Box>
-      <Box
-        className="landing-page-right-container"
-        sx={{
-          position: "relative",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "60%",
-        }}
-      >
+          <Link to={"/explore-tour"}>
+            <Defaultbutton
+              buttonCaption="Explore Now"
+              buttonWidth="155px"
+              buttonHeight="54px"
+              route="/"
+            />
+          </Link>
+        </Box>
         <Box
+          className="landing-page-right-container"
           sx={{
-            display: "grid",
-            gridTemplateColumns: "40% 40%",
-            gridTemplateRows: "1fr",
-            gap: "20px",
-            position: "absolute",
-            bottom: "20px",
-            height: "auto",
-            width: "100%",
-            justifyContent: "end",
+            position: "relative",
+            width: "60%",
           }}
         >
-          <Box
-            component="img"
-            src={image1}
-            alt="Image 1"
-            sx={{
-              gridRow: "1 / span 2",
-              objectFit: "cover",
-            }}
-          />
-          <Box component="img" src={image2} alt="Image 2" />
-          <Box component="img" src={image3} alt="Image 3" />
+          <div className="image-grid">
+            <img src={image1} alt="Left " className="image left-image" />
+            <img src={image2} alt="Top Right " className="image top-right" />
+            <img
+              src={image3}
+              alt="Bottom Right "
+              className="image bottom-right"
+            />
+          </div>
         </Box>
-      </Box>
-    </div>
+      </div>
+    </>
   );
 };
 
